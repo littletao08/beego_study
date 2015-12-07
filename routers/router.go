0f7d290c61c09011ae396d8c0573ed89c6c0e7ef
zhanglida <zhanglida@huoqiu.cn>
2015-12-08 00:45:21 +0800

    add .

--- routers/router.go
+++ routers/router.go
@@ -4,8 +4,6 @@ import (
 	"github.com/astaxie/beego"
 	"beego_study/controllers"
 	_"beego_study/initials"
-	"github.com/astaxie/beego/context"
-	"beego_study/entities"
 )
 func init() {
 	beego.Router("", &controllers.IndexController{},"get:Index")
@@ -20,13 +18,6 @@ func init() {
 	beego.Router("/sponsors/new", &controllers.SponsorController{},"post:New")
 	beego.Router("/article/praise/:id([0-9]",&controllers.ArticleController{},"get:ArticlePraise")
 
-
-	beego.InsertFilter("/*",beego.BeforeRouter,func(ctx *context.Context) {
-		_, ok := ctx.Input.Session("user").(entities.User)
-		if !ok {
-			ctx.Redirect(302, "/login")
-		}
-	})
 }
 
 
