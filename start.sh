237e12b6319f3cfdbccc25ee27eea7590a654b8b
zld406504302 <406504302@qq.com>
2015-12-20 17:45:06 +0800

    add backup feature

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@
 go run main.go> log.log &
 echo $! >beego.pid
 
-tail -f log.log
+time=`date "+%Y%m%d%H%M%S"`
+version=`git log | awk 'NR==1{ print $2 }'`
+tar -cjf /home/zhanglida/data/projects_pakcage_backup/beegostudy/beego-study-"$time"-$version.tar.bz2  .
+
+exit 0 ;
