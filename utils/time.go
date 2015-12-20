7fa31f14d28084561bf82dc699e2b4e231c8eae6
zhanglida <zhanglida@huoqiu.cn>
2015-12-20 17:09:07 +0800

    fixed time show eror

--- utils/time.go
+++ utils/time.go
@@ -1,25 +1,23 @@
 package utils
 import (
 	"time"
-	"github.com/astaxie/beego"
 	"strconv"
 )
 
 func ZhTime(t time.Time) string {
-	nowTimeSecond := time.Now().Second()
-	createTimeSecond := t.Second()
-	beego.Error("nowTimeSecond:",nowTimeSecond,"createTimeSecond:",createTimeSecond)
+	var nowTimeSecond = time.Now().UnixNano()/int64(time.Second)
+	var createTimeSecond = t.UnixNano()/int64(time.Second)
 	diff := nowTimeSecond - createTimeSecond;
 	zhTime := "刚刚";
 	if diff > 0 {
 		if diff < 60 {
-			zhTime = strconv.Itoa(diff) + "秒前";
+			zhTime = strconv.FormatInt(diff,10) + "秒前";
 		}else if diff >= 60 && diff < 60 * 60 {
-			zhTime = strconv.Itoa(diff / 60) + "分钟前";
+			zhTime = strconv.FormatInt(diff / 60,10) + "分钟前";
 		}else if diff >= 60 * 60 && diff < 60 * 60 * 24 {
-			zhTime = strconv.Itoa((diff / (60 * 60))) + "小时前";
+			zhTime = strconv.FormatInt(diff / (60 * 60),10) + "小时前";
 		}else if diff > 60 * 60 * 24 && diff < 60 * 60 * 24 * 365 {
-			zhTime = strconv.Itoa((diff / (60 * 60 * 24))) + "天前";
+			zhTime = strconv.FormatInt((diff / (60 * 60 * 24)),10) + "天前";
 		}else {
 			zhTime = "1年前";
 		}
