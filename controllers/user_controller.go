ab8e84387759bd6edcedbc12732b9c549b362f88
zhanglida <zhanglida@huoqiu.cn>
2016-01-21 11:07:22 +0800

    add log

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -38,6 +38,7 @@ func (c *UserController) Login() {
 func (c *UserController) OauthLogin() {
 	c.Data["showLeftBar"] = false
 	openUser := c.CurrentOpenUser()
+	beego.Debug("openUser:",openUser)
 	if nil == openUser || openUser.UserId == 0 {
 		c.Ctx.Redirect(302, "/")
 		return
