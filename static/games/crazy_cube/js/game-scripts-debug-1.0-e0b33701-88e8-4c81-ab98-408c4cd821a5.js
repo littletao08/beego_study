e0ac400513abc8e1b5580973cd0fb4dc8839cb49
zhanglida <zhanglida@huoqiu.cn>
2016-09-13 09:03:41 +0800

    add touch event

--- static/games/crazy_cube/js/game-scripts-debug-1.0-e0b33701-88e8-4c81-ab98-408c4cd821a5.js
+++ static/games/crazy_cube/js/game-scripts-debug-1.0-e0b33701-88e8-4c81-ab98-408c4cd821a5.js
@@ -176,16 +176,18 @@ GameOverManager.prototype.onKeyDown = function(keyCode) {
 }
 // define a user behaviour
 var Player = qc.defineBehaviour('qc.engine.Player', qc.Behaviour, function () {
-    Player.instance = this ;
-    this.speed = 0 ;
-    this.gravity = 0.6 ;
+    Player.instance = this;
+    this.speed = 0;
+    this.gravity = 0.6;
     this.groundHeight = 100;
-    this.doubleJumped = false ;
-    this.radius = 50 ;
+    this.doubleJumped = false;
+    this.radius = 50;
 
-    this.jumpHeight = 20 ;
-    this.doubleJumpHeight = 15 ;
-    this.smashSpeed = 35 ;
+    this.jumpHeight = 20;
+    this.doubleJumpHeight = 15;
+    this.smashSpeed = 35;
+
+    this.dragStartY = 0;
 }, {
     // fields need to be serialized
 
@@ -193,14 +195,18 @@ var Player = qc.defineBehaviour('qc.engine.Player', qc.Behaviour, function () {
 
 // Called when the script instance is being loaded.
 Player.prototype.awake = function () {
-    GameManager.instance.GAME_START.add(this.onGameStart,this);
-    GameManager.instance.GAME_OVER.add(this.onGameOver,this);
-    GameManager.instance.GAME_RESTART.add(this.onGameRestart,this);
+    GameManager.instance.GAME_START.add(this.onGameStart, this);
+    GameManager.instance.GAME_OVER.add(this.onGameOver, this);
+    GameManager.instance.GAME_RESTART.add(this.onGameRestart, this);
+    // 注册点击响应事件
+    this.game.input.onPointerDown.add(this.onPointerDown, this);
+    //this.game.input.onPointerMove.add(this.onPointerMove,this);
+    this.game.input.onPointerUp.add(this.onPointerUp, this);
 
 };
 
 Player.prototype.onGameStart = function () {
-    this.game.input.onKeyDown.add(this.onKeyDown,this);
+    this.game.input.onKeyDown.add(this.onKeyDown, this);
     this.gameObject.anchoredY = -this.groundHeight;
 };
 
@@ -209,18 +215,18 @@ Player.prototype.onGameRestart = function () {
 };
 
 Player.prototype.onGameOver = function () {
-    this.game.input.onKeyDown.remove(this.onKeyDown,this);
+    this.game.input.onKeyDown.remove(this.onKeyDown, this);
 };
 
 // Called every frame, if the behaviour is enabled.
 Player.prototype.update = function () {
-    if (!GameManager.instance.running){
-        return ;
+    if (!GameManager.instance.running) {
+        return;
     }
 
     this.speed += this.gravity;
 
-    if (this.gameObject.anchoredY < -this.groundHeight && this.gameObject.anchoredY+this.speed>=-this.groundHeight){
+    if (this.gameObject.anchoredY < -this.groundHeight && this.gameObject.anchoredY + this.speed >= -this.groundHeight) {
         this.land();
     }
     this.gameObject.anchoredY += this.speed;
@@ -234,32 +240,52 @@ Player.prototype.update = function () {
 
 Player.prototype.onKeyDown = function (keyCode) {
     //if(keyCode == qc.Keyboard.UP){
-    if(keyCode == 65){
+    if (keyCode == 65) {
         this.jump();
-    //}else if (keyCode == qc.Keyboard.DOWN){
-    }else if (keyCode == 68){
+        //}else if (keyCode == qc.Keyboard.DOWN){
+    } else if (keyCode == 68) {
         this.smash();
     }
 };
 
 Player.prototype.jump = function () {
     if (this.gameObject.anchoredY >= -this.groundHeight) {
-        this.speed =- this.jumpHeight;
-    }else if (!this.doubleJumped){
-        this.speed =- this.doubleJumpHeight;
-        this.doubleJumped = true ;
+        this.speed = -this.jumpHeight;
+    } else if (!this.doubleJumped) {
+        this.speed = -this.doubleJumpHeight;
+        this.doubleJumped = true;
 
     }
 };
 
 
 Player.prototype.smash = function () {
-    this.speed = this.smashSpeed ;
+    this.speed = this.smashSpeed;
 };
 
 Player.prototype.land = function () {
     this.gameObject.Animator.play("land");
 };
+
+
+Player.prototype.onPointerDown = function (id, x, y) {
+    this.dragStartY = y;
+};
+
+
+Player.prototype.onPointerUp = function (id, x, y) {
+    var diff = y - this.dragStartY ;
+    if (diff == 0){
+        return ;
+    }
+    if (y - this.dragStartY < 0.1 ) {
+        this.jump();
+    } else if (y - this.dragStartY < 0.1) {
+        this.smash();
+    }
+};
+
+
 // define a user behaviour
 var ScoreManager = qc.defineBehaviour('qc.engine.ScoreManager', qc.Behaviour, function() {
     ScoreManager.instance = this ;
